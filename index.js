e0e334db98de82e468ffeb85f4be422573d664a4
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-10 17:32:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function getFirstSelector(selector){
+function getFirstSelector(div){
   return document.querySelector('div').innerHTML;
 }
 
 function nestedTarget(selector){
   return document.querySelector('#nested.target').innerHTML;
 }
+
+function increaseRankBy(n)
