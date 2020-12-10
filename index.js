06f0770ddfdb4d9931b53dc06e4b9f54439aac90
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-10 18:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget(){
 
 function deepestChild(){
 var deepnode = document.getElementById("grand-node").querySelectorAll('div');
-return mainnode
+return mainnode[deepnode.length-1]
 }
 
 function increaseRankBy(n){
